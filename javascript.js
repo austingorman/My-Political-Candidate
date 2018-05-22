6d62fe374c827c07908b1b47082c92fe0cb0992e
Austin Gorman <austingorman@Austins-MacBook-Pro.local>
2018-05-22 15:36:10 -0500

    added more updates to all files

--- javascript.js
+++ javascript.js
@@ -1,5 +1,10 @@
+const article = document.querySelector("#main");
 let missionStatementDiv = document.createElement("h2"); 
 let missionStatementContent = document.createTextNode("FUCK YOU FOR BEING POOR");
 missionStatementDiv.appendChild(missionStatementContent);
-const article = document.querySelector("#article").appendChild(missionStatementDiv);
+article.appendChild(missionStatementDiv);
 
+
+const congressionalDistAttr = document.createAttribute("congressional-district");
+congressionalDistAttr.value = "5";
+article.setAttribute(congressionalDistAttr);
