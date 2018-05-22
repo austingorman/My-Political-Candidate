2280ddc96f565770fb0d17e6e16517300a01cb08
Austin Gorman <austingorman@Austins-MacBook-Pro.local>
2018-05-22 15:03:27 -0500

    Added links to the legislation

---
+++ javascript.js
@@ -0,0 +1,5 @@
+let missionStatementDiv = document.createElement("h2"); 
+let missionStatementContent = document.createTextNode("FUCK YOU FOR BEING POOR");
+missionStatementDiv.appendChild(missionStatementContent);
+const article = document.querySelector("#article").appendChild(missionStatementDiv);
+
